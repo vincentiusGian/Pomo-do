c7f5d62049d9752eb3f8c0d1c01d6fce517d47c9
vincentiusGian <126053267+vincentiusGian@users.noreply.github.com>
2026-03-10 04:06:45 +0700

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,33 +4,48 @@ import { CgCompressRight } from "react-icons/cg";
 
 const Modal = () => {
 
-    const [quote, setQuote] = useState({
-      text: "Just do it.",
-      author: "Vincentius Gian Junius",
-    })
+  const [quote, setQuote] = useState({
+    text: "Just do it.",
+    author: "Vincentius Gian Junius",
+  })
 
-   
+  return (
+    <>
+      <button 
+        className="btn btn-lg rounded-md max-w-sm"
+        onClick={() => document.getElementById('my_modal_5').showModal()}
+      >
+        Motivation
+      </button>
 
-    return (
-        <>
-        {/* Open the modal using document.getElementById('ID').showModal() method */}
-<button className="btn btn-lg rounded-md max-w-sm" onClick={()=>document.getElementById('my_modal_5').showModal()}>Motivation</button>
-<dialog id="my_modal_5" className="modal modal-bottom bg-transparent sm:modal-middle">
-  <div className="modal-box shadow-2xl border-4 rounded-md border-white">
-    <h3 className="font-bold text-lg">Semangat Calon Mahasiswa UofT Mississauga!</h3>
-    <p className="py-4">{quote.text}</p>
-    <p> - {quote.author.split(',')[0]}</p>
-    <div className="modal-action">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-     
-        <button className="btn btn btn-sm btn-circle btn-ghost absolute right-2 top-2"><CgCompressRight /></button>
-      </form>
-    </div>
-      
-  </div>
-</dialog></>
-    )
+      <dialog id="my_modal_5" className="modal modal-bottom bg-transparent sm:modal-middle">
+        <div className="modal-box shadow-2xl border-4 rounded-md border-white">
+
+          <h3 className="font-bold text-lg">
+            Semangat Calon Mahasiswa UofT Mississauga!
+          </h3>
+
+          <p className="py-4">{quote.text}</p>
+          <p>- {quote.author}</p>
+
+          <div className="modal-action">
+            <form method="dialog">
+
+              <div className="btn btn-sm btn-circle hover:rounded-full">
+                <TfiReload />
+              </div>
+
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                <CgCompressRight />
+              </button>
+
+            </form>
+          </div>
+
+        </div>
+      </dialog>
+    </>
+  )
 }
 
 export default Modal
